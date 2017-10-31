31028a6498f3b4cb0271c47d69cd17f65fe2adb8
BredeFK <bredefk@stud.ntnu.no>
2017-11-01 00:33:31 +0100

    Implemented handler for average

--- handlers.go
+++ handlers.go
@@ -85,7 +85,6 @@ func HandleLatest (w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-
 		defer r.Body.Close()
 
 		if payload.BaseCurrency != "EUR" {
@@ -93,11 +92,12 @@ func HandleLatest (w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	case "GET":
+		// Setting a default value if the user opens the program in a webbrowser
 		payload.BaseCurrency = "EUR"
 		payload.TargetCurrency = "NOK"
 
 	default:
-		http.Error(w, "Method has to be POST", http.StatusMethodNotAllowed)
+		http.Error(w, "Method has to be POST (or GET)", http.StatusMethodNotAllowed)
 		return
 	}
 
@@ -112,16 +112,75 @@ func HandleLatest (w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	rate := currency.Rates[payload.TargetCurrency]
+	fmt.Fprint(w, rate)
 	/*
 	rateString := fmt.Sprint(rate)
 	text := "The rate between " + payload.BaseCurrency + " and " + payload.TargetCurrency + " is: " + rateString
 	DiscordOperator( text , DiscordURL_notAbot)
 	*/
-	fmt.Fprint(w, rate)
 }
 
 func HandleAverage (w http.ResponseWriter, r *http.Request) {
 
+	totalDays := 3
+	tdFloat := float64(totalDays)
+	payload := Payload{}
+	var day [365]float64
+
+	switch r.Method {
+	case "POST":
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+
+		if payload.BaseCurrency != "EUR" {
+			http.Error(w, payload.BaseCurrency+" is not implemented to be baseCurrency", http.StatusNotImplemented)
+			return
+		}
+	case "GET":
+		// Setting a default value if the user opens the program in a webbrowser
+		payload.BaseCurrency = "EUR"
+		payload.TargetCurrency = "NOK"
+
+	default:
+		http.Error(w, "Method has to be POST (or GET)", http.StatusMethodNotAllowed)
+		return
+	}
+
+	tempToday := time.Now().Local()
+	today := tempToday.Format("2006-01-02")
+
+	db := SetupDB()
+	sum := 0.0000
+
+	for i := 0; i < totalDays; i++{
+
+		currency, ok := db.GetLatest(today)
+
+		if ok == false{
+			http.Error(w, "There isn't any data for all the days yet", 404)
+			return
+		}
+
+		day[i] = currency.Rates[payload.TargetCurrency]
+
+		tempToday.AddDate(0,0,-1)
+		today = tempToday.Format("2006-01-02")
+
+		sum += day[i]
+	}
+
+	average := sum / tdFloat
+	fmt.Fprint(w, average)
+	/*
+	days := strconv.Itoa(totalDays)
+	averageString := fmt.Sprint(average)
+	text := "The average rate over the " + days + " days between " + payload.BaseCurrency + " and " + payload.TargetCurrency + " is: " + averageString
+	DiscordOperator( text , DiscordURL_notAbot)
+	*/
 }
 
 func HandleAdd (w http.ResponseWriter, r *http.Request) {	// TODO : make automatic
