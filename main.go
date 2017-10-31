e925e83496443145d66e051763b9c4ad3bd5ebfe
BredeFK <BredeFK@stud.ntnu.no>
2017-10-31 12:37:31 +0100

    Added some files

---
+++ main.go
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////////////////////////////
+// 		   AUTHOR: 	Brede Fritjof Klausen		  				  //
+// STUDENT NUMBER: 	473211						 				 //
+// 		  SUBJECT: 	IMT2681 Cloud Technologies					//
+//=============================================================//
+//	SOURCES:												  //
+// * 														 //
+// *														//
+/////////////////////////////////////////////////////////////
+package main
+
+import (
+	"net/http"
+)
+
+
+func main() {
+
+//	port := os.Getenv("PORT")
+// 	http.ListenAndServe(":" + port, nil)
+
+	http.HandleFunc("/", HandleWebhook)
+	http.ListenAndServe("localhost:8080", nil)
+
+//	text := "Sup mah dudes!"
+//	DiscordOperator(text, DiscordURL_notAbot)
+}
