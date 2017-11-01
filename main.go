2ce0e69ec50049c3fada29c348845aac52e00e75
BredeFK <BredeFK@stud.ntnu.no>
2017-11-01 19:42:49 +0100

    Deployed MongoDB

--- main.go
+++ main.go
@@ -3,13 +3,14 @@
 // 		  SUBJECT: 	IMT2681 Cloud Technologies													    \\
 //==================================================================================================\\
 //	SOURCES:												 									    \\
-// * https://stackoverflow.com/questions/38127583/get-last-inserted-element-from-mongodb-in-golang  \\														 //
+// * https://stackoverflow.com/questions/38127583/get-last-inserted-element-from-mongodb-in-golang  \\
 // * 																							    \\
 //==================================================================================================\\
 package main
 
 import (
 	"net/http"
+	"time"
 	"os"
 )
 
@@ -17,19 +18,18 @@ func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/", HandleWebhook)
 	http.HandleFunc("/latest", HandleLatest)
-	http.HandleFunc("/add", HandleAdd)		// TODO : Remove this to automatic
+	http.HandleFunc("/add", HandleAdd)						// TODO : Remove this to automatic
 	http.HandleFunc("/average", HandleAverage)
 	http.HandleFunc("/evaluationtrigger", HandleTestTrigger)
 	go http.ListenAndServe(":" + port, nil)
 //	go http.ListenAndServe("localhost:8080", nil)
 
-/*		// TODO : deploy mongodb and the uncomment this
 	for {
-		delay := time.Minute * 15
-		time.Sleep(delay)
-
 		DailyCurrencyAdder()
 		CheckTrigger()
+
+		delay := time.Hour * 24
+		time.Sleep(delay)
 	}
-*/
+
 }
