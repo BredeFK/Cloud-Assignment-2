9d94021ee6ec6957cf5211fb9f8d3e68ff9f080e
BredeFK <BredeFK@stud.ntnu.no>
2017-11-03 22:45:54 +0100

    Test a new solution for timer

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "Oblig2_Heroku/gofiles"
+import (
+	"Oblig2_Heroku/gofiles"
+	"time"
+)
 
 func main(){
 
-	gofiles.DailyCurrencyAdder()
-	gofiles.CheckTrigger()
+	// Heroku scheduler =  16:30
+
+	triggerTime := "2300"
+
+	for {
+		tempTime := time.Now().Local()
+		timeNow := tempTime.Format("1504")	// format to 2400
+
+		if timeNow == triggerTime {
+			gofiles.DailyCurrencyAdder()
+			gofiles.CheckTrigger()
+		}
+		delay := time.Minute
+		time.Sleep(delay)
+	}
 }
 
 
