cb133f6a157856559d9ba3e0fcb8047f4108b3aa
BredeFK <BredeFK@stud.ntnu.no>
2017-11-07 14:40:38 +0100

    Possible fix off daily chekker

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -6,19 +6,11 @@ import (
 
 func main(){
 
-	// Heroku scheduler =  17:00 UTC
+	// Heroku scheduler =  15:30 UTC
 
-	/*
-	const triggerTime = "17"
-
-	tempTime := time.Now().UTC()
-	timeNow := tempTime.Format("15")	// format to 2400
-
-	if timeNow == triggerTime {
-	*/
 		gofiles.DailyCurrencyAdder()
 		gofiles.CheckTrigger()
-	// }
+
 
 }
 
