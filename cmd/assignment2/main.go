f59e845750107b31c487dfd5eae78ff794bffacf
BredeFK <BredeFK@stud.ntnu.no>
2017-11-09 02:04:33 +0100

    Forgot uncommentin localhost

--- cmd/assignment2/main.go
+++ cmd/assignment2/main.go
@@ -14,14 +14,15 @@ import (
 	"bitbucket.org/Brede_F_Klausen/assignment2_cloud/gofiles"
 	"net/http"
 
+	"os"
 )
 
 func main() {
-//	port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	http.HandleFunc("/", gofiles.HandleWebhook)
 	http.HandleFunc("/latest", gofiles.HandleLatest)
 	http.HandleFunc("/average", gofiles.HandleAverage)
 	http.HandleFunc("/evaluationtrigger", gofiles.HandleTestTrigger)
-//	http.ListenAndServe(":"+port, nil)
-	http.ListenAndServe("localhost:8080", nil)
+	http.ListenAndServe(":"+port, nil)
+//	http.ListenAndServe("localhost:8080", nil)
 }
